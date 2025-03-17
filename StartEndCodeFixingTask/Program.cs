be125e5ae5a31a0a0c22a12df9c3545e08eb7920
Surabhi <21SSrivathsa@heckgrammar.co.uk>
2025-03-17 20:42:50 +0000

    improved

--- StartEndCodeFixingTask/Program.cs
+++ StartEndCodeFixingTask/Program.cs
@@ -10,6 +10,36 @@
             // Put your evidence where it says Do the coding task in C#
 
 
+            bool validChoice = false;
+            int difference;
+            int startYear;
+            int endYear;
+            do
+            {
+                difference = -1;
+                Console.WriteLine("enter a start year");
+                startYear = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter an end year");
+                endYear = Convert.ToInt32(Console.ReadLine());
+                if (startYear >= endYear)
+                {
+                    Console.WriteLine("start year must be before end year");
+                }
+                else
+                {
+                    if (startYear > 2000)
+                    {
+                        Console.WriteLine("start year must be before 2000");
+                    }
+                    else
+                    {
+                        validChoice = true;
+                    }
+                }
+
+            } while (validChoice==true);
+            difference = endYear - startYear;
+            Console.WriteLine(difference + " years");
         }
     }
 }
